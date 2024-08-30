5c56a4541b9745b7b66c2bc2e772dd70d5bd727a
AsifH-28 <asifh@ivoyant.com>
2024-08-30 20:32:05 +0530

    Added Styling

--- react-project/src/components/BreadCrum/BreadCrum.tsx
+++ react-project/src/components/BreadCrum/BreadCrum.tsx
@@ -1,23 +1,33 @@
-import { Link } from "react-router-dom";
-
-interface BreadCrumParams{
-    path:string,
-    label:string,
+import { Link } from 'react-router-dom';
+import "./BreadCrum.css"
+interface BreadCrumParams {
+  path: string;
+  label: string;
+  nested: {
+    path: string;
+    label: string;
+  };
 }
-export type items ={
-    items:BreadCrumParams[];
-}
 
-export default function BreadCrum({items}:items) {
-    console.log(items)
+export type items = {
+  items: BreadCrumParams[];
+};
+
+export default function BreadCrum({ items }: items) {
   return (
-<nav>
-    <div style={{display:"flex",justifyContent:"flex-start",alignItems:"flex-start", flexDirection:"column"}}>
-        {items.map((i:BreadCrumParams)=>{
-            return(<Link style={{margin:"10px"}} key={Math.random()+23} to={i.path}>{i.label}</Link>
-            )
-        })}
-    </div>
-</nav>
-  )
+    <nav>
+      <div className="breadcrumb-container">
+        {items.map((i: BreadCrumParams) => (
+          <div key={i.path} className="breadcrumb-item">
+            <Link className="parent-link" to={i.path}>
+              {i.label}
+            </Link>
+            <Link className="child-link" to={i.nested.path}>
+              {i.nested.label}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </nav>
+  );
 }
