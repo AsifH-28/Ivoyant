d6647ec85db41ba90a9a0f037cf3233b28218be1
AsifH-28 <asifh@ivoyant.com>
2024-08-30 20:31:34 +0530

    Added Styling

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -5,16 +5,32 @@ import BreadCrum from "./components/BreadCrum/BreadCrum";
 const items ={
   item:[{
     path:"/home",
-    label:"home"
+    label:"home",
+    nested:{
+      path:"/home/profile",
+      label:"profile",
+    }
   },{
     path:"/about",
-    label:"About"
+    label:"About",
+    nested:{
+      path:"/About/Members",
+      label:"Members",
+    }
   },{
     path:"/Account",
-    label:"Account"
+    label:"Account",
+    nested:{
+      path:"/Account/Documents",
+      label:"Documents",
+    }
   },{
-    path:"/Account",
-    label:"Account"
+    path:"/TermsAndConditions",
+    label:"Terms And Conditions",
+    nested:{
+      path:"/TermsAndConditions/Rules",
+      label:"Rules",
+    }
   }]
 }
 
