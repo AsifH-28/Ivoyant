8f657ee13232aea60dd09ce3a931d84f7f95b75a
AsifH-28 <asifh@ivoyant.com>
2024-09-24 15:34:56 +0530

    removed unwanted code

--- react-project/src/worker.js
+++ react-project/src/worker.js
@@ -1,13 +1,14 @@
 self.onmessage = function (event) {
-    const result = heavyComputation(event.data);
+  const result = heavyComputation(event.data);
+  setTimeout(() => {
     self.postMessage(result);
-  };
-  
-  function heavyComputation(data) {
-    let sum = 0;
-    for (let i = 0; i < data; i++) {
-      sum += i;
-    }
-    return sum;
+  }, 8000);
+};
+
+function heavyComputation(data) {
+  let sum = 0;
+  for (let i = 0; i < data; i++) {
+    sum += i;
   }
-  
+  return sum;
+}
